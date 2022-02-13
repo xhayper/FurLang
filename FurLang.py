3e5b962cd451070c7c9e30e9e152c98fb7b5009e
xhayper <hayper1919@gmail.com>
2022-02-13 17:30:49 +0700

    working stuff

--- FurLang.py
+++ FurLang.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
+from Interperter import Interperter
 from Lexer import Lexer
 import sys
 
 if __name__ == "__main__":
     content = open(sys.argv[1], "r").read()
-    Lex = Lexer(content)
-    # Lex.make_tokens()
-    print(Lex.make_tokens())
+    Interperter(Lexer(content).make_tokens()).run()
--- Interperter/__init__.py
+++ Interperter/__init__.py
@@ -1,4 +1,32 @@
-class Interperter(object):
+from Common.Token import *
 
-    def run(tokenList):
-        print("Hi!")
+# TODO: Make an actual interperter, this is a draft
+
+class Interperter:
+    tokenList = []
+    index = 0
+
+    def __init__(self, tokenList):
+        self.tokenList = tokenList
+        self.index = -1
+
+    def advance(self):
+        self.index += 1
+        if not self.haveNext():
+            return None
+        return self.tokenList[self.index]
+
+    def haveNext(self):
+        return len(self.tokenList) > self.index
+
+    def run(self):
+        while self.haveNext():
+            token = self.advance()
+            if token.tokenType == Keyword.AWOO:
+                content = self.advance()
+                if not (content.tokenType in [Constant.BOOL, Constant.INT]):
+                    raise SyntaxError(token.value)
+                else:
+                    print(content.value)
+            self.index += 1
+            
