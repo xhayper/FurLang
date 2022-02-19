ad786638ed42da5390e82f6a672c120e4893ef18
xhayper <hayper1919@gmail.com>
2022-02-19 14:31:50 +0700

    Removed __ignore__ folder

--- __ignore__/Python/FurLang.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from Interperter import Interperter
-from Lexer import Lexer
-import sys
-
-if __name__ == "__main__":
-    content = open(sys.argv[1], "r").read()
-    Interperter(Lexer(content).make_tokens()).run()
